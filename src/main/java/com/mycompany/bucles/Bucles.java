4c508ddf2e271304debe32df1f2e9b63bd998a8f
Ana Morales <anab_morales@hotmail.com>
2023-11-13 21:13:44 +0100

    Incluyo autor en clase

---
+++ src/main/java/com/mycompany/bucles/Bucles.java
@@ -0,0 +1,19 @@
+package com.mycompany.bucles;
+/**
+ * Esta es la clase Bucles.
+ * Muestra del 1 al 10.
+ * 
+ * Autor: Ana Belén Morales Abad
+ */
+public class Bucles{
+public static void main(String[] args){
+//int i=0;
+//while(i<=10){
+//System.out.println(i);
+//i++;
+//}
+for(int i=0; i<=10;i++){
+System.out.println(i);
+}
+}
+}
